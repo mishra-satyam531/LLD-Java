27c8e3f9facf49bc13e8b07238b40d0395958efd
mishra-satyam531 <stym5674@gmail.com>
2026-01-23 00:43:08 +0530

    Feat: Add File class with constructor and printContents method

---
+++ Pattern/CompositeDesignPattern/File.java
@@ -0,0 +1,14 @@
+package Pattern.CompositeDesignPattern;
+
+public class File {
+    String fileName;
+
+    public File(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void printContents() {
+        System.out.println("File name: " + fileName);
+    }
+    
+}
